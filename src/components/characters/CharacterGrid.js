0f09fc477a43e699be307458a58af82026b228db
Elisa <elisacanyelles@gmail.com>
2021-10-24 14:41:40 +0200

    created characterItem comp. added html and props characters

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import { nanoid } from "nanoid";
+import CharacterItem from './CharacterItem'
 
 const CharacterGrid = ( {items, isLoading }) => {
-    return isLoading ? <h1>Loading...</h1> : <section className='cards'>
-        {items.map(item => (<h1 key={nanoid()}>{item.name}</h1>) )}</section>
+    return isLoading ? (
+    <h1>Loading...</h1>
+    ) : (
+    <section className='cards'>
+        {items.map(item => (
+        <CharacterItem key={nanoid()} item={item}></CharacterItem>
+        )) }
+        </section>
+   )
 }
 
 export default CharacterGrid
