8d11d790ff656f6168e14473fe7efbf3808dccdf
Elisa <elisacanyelles@gmail.com>
2021-10-24 13:01:41 +0200

    useEffect & HTTP request, taking the global State and passing it into charactergrid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(()=> {
+    const fetchItems = async () => {
+      const result = await axios(`https://projectheist.herokuapp.com/characters/1`)
+
+      console.log(result.data)
+
+      //we've got the data, so we put setIsLoading as false.
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems()
+
+  }, [])
+
   return (
     <div className="container">
       <Header/>
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
